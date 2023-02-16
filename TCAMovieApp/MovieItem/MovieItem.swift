ae75dbb4cc84b3bf696ba0906e787f2517838162
MinaMaherR <Mina.maher6666@gmail.com>
2023-02-16 22:23:19 +0200

    feat: update MovieItem testItem

--- TCAMovieApp/MovieItem/MovieItem.swift
+++ TCAMovieApp/MovieItem/MovieItem.swift
@@ -7,16 +7,20 @@
 
 import Foundation
 
-protocol Testable {
-    static var testItem: Self { get }
-}
-
-struct MovieItem: Testable {
+struct MovieItem {
     let id: Int
     let name: String
     let image: String
     
-    static var testItem: MovieItem {
-        .init(id: 1, name: "Movie Title", image: "popcorn")
+    static func testItem(id: Int = 1) -> MovieItem {
+        .init(id: id, name: "Movie Title", image: "popcorn")
+    }
+}
+
+extension Sequence where Element == MovieItem {
+    static func testList(size: Int = 10) -> [MovieItem] {
+        (1...size).map {
+            MovieItem(id: $0, name: "Movie \($0)", image: "popcorn")
+        }
     }
 }
