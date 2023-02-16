be58077b0df0376508bd67dd8f4409b187b52c13
MinaMaherR <Mina.maher6666@gmail.com>
2023-02-17 00:06:47 +0200

    feat: basic apiclient

---
+++ TCAMovieApp/Base/APIClient.swift
@@ -0,0 +1,47 @@
+//
+//  APIClient.swift
+//  TCAMovieApp
+//
+//  Created by Mina Maher on 16/02/2023.
+//
+
+import Foundation
+
+enum APIError: Error {
+    case invalidUrl
+}
+
+enum RequestMethod: String {
+    case get = "GET"
+}
+
+func apiRequest(method: RequestMethod = .get, path: String, queryItems: [URLQueryItem]? = nil) async throws -> Data {
+    guard
+        var url = URL(string: Constants.baseUrl)
+    else {throw APIError.invalidUrl}
+    
+    url.append(path: path)
+    url.append(queryItems: queryItems ?? [])
+    var request: URLRequest = URLRequest(url: url)
+    request.httpMethod = method.rawValue
+    
+    let headers: [String: String] = [
+        Constants.tokenkey: Constants.tokenValue
+    ]
+    headers.forEach({
+        request.setValue($1, forHTTPHeaderField: $0)
+    })
+    
+    let (data, _) = try await URLSession.shared.data(for: request)
+    
+    logData(url: url, data: data)
+    
+    return data
+}
+
+func logData(url: URL, data: Data) {
+    #if DEBUG
+    print("========= URL=========\n\(url.absoluteString)")
+    print("========= response =========\n\(String(describing: String(data: data, encoding: .utf8)))")
+    #endif
+}
---
+++ TCAMovieApp/Base/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  TCAMovieApp
+//
+//  Created by Mina Maher on 16/02/2023.
+//
+
+import Foundation
+
+struct Constants {
+    static let baseUrl = "https://api.themoviedb.org/3/"
+    
+    static let tokenkey = "Authorization"
+    static let tokenValue = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZTA5YmIyNDZkMDVmYTU1YzFiNWVkYzBmYTk1N2RkNSIsInN1YiI6IjVlMDllYmY5MmQxZTQwMDAxODY4NGZiOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l4CbPTYBXCNTxdvp1MUK0iGKs8kQpYxhbL0LPyVBZuI"
+}
