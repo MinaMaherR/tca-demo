66d1913b849db3187526acdd090ed502aa9acea5
MinaMaherR <Mina.maher6666@gmail.com>
2023-02-16 22:24:17 +0200

    feat: MoviesListView

---
+++ TCAMovieApp/MoviesList/MoviesListView.swift
@@ -0,0 +1,29 @@
+//
+//  MoviesListView.swift
+//  TCAMovieApp
+//
+//  Created by Mina Maher on 16/02/2023.
+//
+
+import SwiftUI
+
+struct MoviesListView: View {
+    var movies: [MovieItem]
+        
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 150), spacing: 0, alignment: .center)]) {
+                ForEach(movies, id: \.id) { movie in
+                    MovieItemView(movie: movie)
+                        .frame(height: 150)
+                }
+            }
+        }
+    }
+}
+
+struct MoviesListView_Preview: PreviewProvider {
+    static var previews: some View {
+        MoviesListView(movies: .testList())
+    }
+}
