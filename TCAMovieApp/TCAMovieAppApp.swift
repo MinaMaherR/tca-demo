12e570806c1e8dbc115431aa76df998408e8a278
MinaMaherR <Mina.maher6666@gmail.com>
2023-02-16 18:58:20 +0200

    init: init commit

---
+++ TCAMovieApp/TCAMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAMovieAppApp.swift
+//  TCAMovieApp
+//
+//  Created by Mina Maher on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TCAMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
